9d99c349d90d30290fade1ac69a30ad7151fc6ed
MattoYuzuru <matveybobr937@gmail.com>
2023-11-17 22:53:29 +0300

    Final Node Movement

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.locals import *
+
 from constants import *
+from nodes import NodeGroup
 from pacman import Pacman
 
 
@@ -19,7 +21,9 @@ class GameController(object):
 
     def startGame(self):
         self.setBackground()
-        self.pacman = Pacman()
+        self.nodes = NodeGroup()
+        self.nodes.setupTestNodes()
+        self.pacman = Pacman(self.nodes.nodeList[0])
 
     def update(self):
         dt = self.clock.tick(30) / 1000.0
@@ -35,6 +39,7 @@ class GameController(object):
 
     def render(self):
         self.screen.blit(self.background, (0, 0))
+        self.nodes.render(self.screen)
         self.pacman.render(self.screen)
 
         pygame.display.update()
