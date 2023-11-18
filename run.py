7b39d6ddd95c4a34230c5b950e1f3433ebb8c781
MattoYuzuru <matveybobr937@gmail.com>
2023-11-18 23:41:27 +0300

    Added pellets and power pellets

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ from constants import *
 from nodes import NodeGroup
 from pacman import Pacman
 
+from pellets import PelletGroup
+
 
 class GameController(object):
     def __init__(self):
@@ -24,10 +26,12 @@ class GameController(object):
         self.nodes = NodeGroup("maze1.txt")
         self.nodes.setPortalPair((0, 17), (27, 17))
         self.pacman = Pacman(self.nodes.getStartTempNode())
+        self.pellets = PelletGroup("maze1.txt")
 
     def update(self):
         dt = self.clock.tick(30) / 1000.0
         self.pacman.update(dt)
+        self.pellets.update(dt)
 
         self.checkEvents()
         self.render()
@@ -40,6 +44,7 @@ class GameController(object):
     def render(self):
         self.screen.blit(self.background, (0, 0))
         self.nodes.render(self.screen)
+        self.pellets.render(self.screen)
         self.pacman.render(self.screen)
 
         pygame.display.update()
