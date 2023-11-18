03467510a2ddb3dad3200b621140e1ae377ecced
MattoYuzuru <matveybobr937@gmail.com>
2023-11-18 22:50:53 +0300

    Added Portal Feature

--- nodes.py
+++ nodes.py
@@ -9,7 +9,7 @@ import numpy as np
 class Node(object):
     def __init__(self, x, y):
         self.position = Vector2(x, y)
-        self.neighbors = {UP: None, DOWN: None, LEFT: None, RIGHT: None}
+        self.neighbors = {UP: None, DOWN: None, LEFT: None, RIGHT: None, PORTAL:None}
 
     def render(self, screen):
         for n in self.neighbors.keys():
@@ -89,6 +89,13 @@ class NodeGroup(object):
 
     def constructKey(self, x, y):
         return x * TILEWIDTH, y * TILEHEIGHT
+
+    def setPortalPair(self, pair1, pair2):
+        key1 = self.constructKey(*pair1)
+        key2 = self.constructKey(*pair2)
+        if key1 in self.nodesLUT.keys() and key2 in self.nodesLUT.keys():
+            self.nodesLUT[key1].neighbors[PORTAL] = self.nodesLUT[key2]
+            self.nodesLUT[key2].neighbors[PORTAL] = self.nodesLUT[key1]
 
     def render(self, screen):
         for node in self.nodesLUT.values():
